dd728205b10eb1e1527510e36bf0cbb8975c5d31
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-16 16:33:23 +0900

    Add: test

--- src/main/java/com/example/TestController.java
+++ src/main/java/com/example/TestController.java
@@ -1,2 +1,16 @@
-package com.example;public class TestController {
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping(value="/test")
+    public UserDto test(){
+        UserDto userDto = new UserDto();
+        userDto.setAge(20);
+        userDto.setName("spring");
+
+        return userDto;
+    }
 }
--- src/main/java/com/example/UserDto.java
+++ src/main/java/com/example/UserDto.java
@@ -1,2 +1,13 @@
-package com.example;public class UserDto {
+package com.example;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserDto {
+    private String name;
+    private Integer age;
 }
